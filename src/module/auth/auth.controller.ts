db6ffc74d031c5b2039be8e01f626add0bee3f44
hridayroy19 <hrhridoyroy503@gmail.com>
2024-12-25 13:12:12 +0600

    creat validaton requst

--- src/module/auth/auth.controller.ts
+++ src/module/auth/auth.controller.ts
@@ -3,34 +3,31 @@ import catchAsync from "../../utils/catchAsync";
 import sendResponse from "../../utils/sendResponse";
 import { AuthServer } from "./auth.service";
 
-const register = catchAsync(async (req , res)=>{
-     const result = await AuthServer.regiserIntoDb(req.body)
+const register = catchAsync(async (req, res) => {
+    const result = await AuthServer.regiserIntoDb(req.body)
 
-     sendResponse(res,{
-        status:true,
-        statusCode:httpStatus.CREATED,
-        message:"register is successfully",
+    sendResponse(res, {
+        status: true,
+        statusCode: httpStatus.CREATED,
+        message: "register is successfully",
         data: result
-     })
-     }
+    })
+}
 )
 
 
-const login = catchAsync(async (req , res)=>{
+const login = catchAsync(async (req, res) => {
     const result = await AuthServer.loginIntoDb(req.body)
 
-    sendResponse(res,{
-       status:true,
-       statusCode:httpStatus.ACCEPTED,
-       token:result.token,
-       message:"Login is successfully",
-       data:result.veryfiUser
+    sendResponse(res, {
+        status: true,
+        statusCode: httpStatus.ACCEPTED,
+        token: result.token,
+        message: "Login is successfully",
+        data: result.veryfiUser
     })
-    }
+}
 )
-
-
-
 
 export const AuthController = {
     register,
